8328e4d4a33f0d7659503b193ca993806c28fba0
AmberJ <amberj@imac-server.local>
2017-07-19 12:18:19 +1200

    Initial Set up Markdown Basics

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello World")
+
+print(str)
+
+print("I have Learned the following:")
+print("What features make Swift a modern and safe laungage")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make Swift fun and simple")
+
+var song = "Lover Lover"
+var artist = "Sonya Dada"
+
+print(song)
+print(artist)
+
+print(" Lover you don't treatme good no more")
+print("Lover you dont love me every night, and it hurts me so bad cos you don't treat me right"
+print("Lover lover lover you don't treat me good no more, good no more")
+
