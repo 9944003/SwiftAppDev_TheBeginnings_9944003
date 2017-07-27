9a99702e9aa23500249642e9e84d6140cfa85d4c
AmberJ <amberj@imac-server.local>
2017-07-27 14:47:03 +1200

    Add Getting started swift files, nothing vital to add Weekly Journal

---
+++ GettingStartedDebug/GettingStartedDebug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GettingStartedDebug
+//
+//  Created by AmberJ on 27/07/17.
+//  Copyright © 2017 AmberJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
