dbe8543ba63a899027f6628ca8acfe71f9bab3d3
AmberJ <amberj@imac-server.local>
2017-07-31 18:20:57 +1200

    Add Unit 2 Started using Xcode and looking at UIKit

--- IBBasics/IBBasics/ViewController.swift
+++ IBBasics/IBBasics/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func ButtonPressed(_ sender: Any) { print("The button was pressed")
+        
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
